f68b067cf170760e2c4387c3114c567632c16b2f
DouglasFlorido <crascaroth@gmail.com>
2021-05-06 14:40:48 -0300

    Especialidade adicionada

--- back-clinica-saude/src/Routes/userRouter.ts
+++ back-clinica-saude/src/Routes/userRouter.ts
@@ -7,3 +7,4 @@ const userController = new UserController();
 
 userRouter.post("/signup/medic", userController.signupMedic)
 userRouter.post("/signup/patient", userController.signupPatient)
+userRouter.post("/signuo/specialty", userController.signupSpecialty)
